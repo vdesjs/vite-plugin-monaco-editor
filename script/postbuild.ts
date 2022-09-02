22cccd1c66058eb75aa45d3890399db6af6adf6f
xiaoyi.gao <xiaoyi.gao@mihoyo.com>
2022-09-02 13:09:57 +0800

    feat: add export for monacoEditorPlugin

---
+++ script/postbuild.ts
@@ -0,0 +1,22 @@
+import { resolve, basename } from 'path'
+import { promises as fs } from 'fs'
+import { fileURLToPath } from 'url'
+import fg from 'fast-glob'
+
+async function run() {
+    // fix cjs exports
+    const files = await fg('*.js', {
+        ignore: ['chunk-*'],
+        absolute: true,
+        cwd: resolve(fileURLToPath(import.meta.url), '../../dist'),
+    })
+    for (const file of files) {
+        console.log('[postbuild]', basename(file))
+        let code = await fs.readFile(file, 'utf8')
+        code = code.replace('exports.default =', 'module.exports =')
+        code += 'exports.default = module.exports;'
+        await fs.writeFile(file, code)
+    }
+}
+
+run()
